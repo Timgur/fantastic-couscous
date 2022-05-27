51749828afbacbd06aa1f436cbc6faf1e5b4e758
Tony Guglielmi <1916060+Timgur@users.noreply.github.com>
2022-05-27 13:49:25 -0400

    add dynamic port

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json({ extended: true }));
 
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
     let event = request.body;
 
     console.log(event);
 
     response.send();
-});
+  }
+);
 
-app.listen(4242, () => console.log('Running on port 4242'));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, () => console.log("Running on port: " + port));
